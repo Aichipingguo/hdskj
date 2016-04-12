83c5df456c8801298a8f46f7184b4d6198abb2c0
zhzhengkeke <zhengge241@126.com>
2016-04-12 21:45:25 +0800

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "user/sessions",
+    registrations: "user/registrations",
+    passwords: "user/passwords",
+  }
   root :to=>'home#index'
 
   get 'main' => 'admin/main#index'
